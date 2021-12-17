83d677635dd7bd6a278241e89c9003d230ce93ad
Vlad Stan <stan.v.vlad@gmail.com>
2021-12-17 12:03:05 +0200

    chore: version bump

--- src/ecpair.js
+++ src/ecpair.js
@@ -84,11 +84,11 @@ function ECPairFactory(ecc) {
     }
     get publicKey() {
       if (!this.__Q) {
-        // @ts-ignore: it is not possible for both `__Q` and `__D` to be undefined.
+        // @ts-ignore: it is not possible for both `__Q` and `__D` to be `undefined` at the same time.
         // The factory methods guard for this.
         const p = ecc.pointFromScalar(this.__D, this.compressed);
         // @ts-ignore: it is not possible for `p` to be null.
-        // `fromPrivateKey()` checks that `__D` is a valid scalar
+        // `fromPrivateKey()` checks that `__D` is a valid scalar.
         this.__Q = Buffer.from(p);
       }
       return this.__Q;
